a2f4bddbc936bada3e0f89bf2f8c6ba0dd8fe76d
alisha musamabik <alisha.musamabik@gmail.com>
2020-06-02 10:25:38 +0100

    start project TDD practice2

---
+++ src/fizzbuzz.test.js
@@ -0,0 +1,13 @@
+import { describe } from "mocha";
+
+describe('fizzCalcuate', () => {
+    it('number should be multiple of three should output "Fizz"',() =>{
+        expect()
+    })
+
+})
+
+
+// it('Should render a <table />', () => {
+//     expect(wrapper.find('table').length).toEqual(1);
+// })
